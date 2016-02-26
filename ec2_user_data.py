e1b138ee5720d01769e62d068eadb5e61d4dba66
Jonathan Mettes <jonathan@jmettes.com>
2016-02-26 16:00:55 +1100

    Add user-data example

--- ec2_user_data.py
+++ ec2_user_data.py
@@ -4,34 +4,33 @@ import botocore.exceptions
 
 ec2 = boto3.resource('ec2')
 
-# allow SSH access
+# allow HTTP access
 try:
-    security_group = ec2.create_security_group(GroupName='ssh_access',
+    security_group = ec2.create_security_group(GroupName='http_access',
                                                Description='pug talk')
     security_group.authorize_ingress(IpProtocol='tcp',
                                      CidrIp='0.0.0.0/0',
-                                     FromPort=22,
-                                     ToPort=22)
+                                     FromPort=80,
+                                     ToPort=80)
 except botocore.exceptions.ClientError:
     pass
 
 
-# create SSH key
-try:
-    key = ec2.create_key_pair(KeyName='cpug')
-    with open('cpug.pem', 'w') as file:
-        file.write(key.key_material)
-except botocore.exceptions.ClientError:
-    pass
+# user-data script to install web server
+user_data="""#!/bin/bash
+# install web server
+yum install httpd -y
+echo "hello world!" > /var/www/html/index.html
+service httpd start
+"""
 
-
-# launch an instance with SSH key + Security Group (port 22)
+# launch an instance with
 instances = ec2.create_instances(ImageId='ami-11032472',
                                  InstanceType='t2.micro',
                                  MinCount=1,
                                  MaxCount=1,
-                                 SecurityGroupIds=['ssh_access'],  # <-- security group
-                                 KeyName='cpug')                   # <-- ssh key
+                                 SecurityGroupIds=['http_access'],
+                                 UserData=user_data)
 
 
 print('launching')
@@ -47,7 +46,7 @@ while True:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(1)
     try:
-        s.connect((ip_address, 22))
+        s.connect((ip_address, 80))
         break
     except:
         pass
@@ -55,6 +54,5 @@ while True:
     s.close()
     time.sleep(1)
 
-print('running')
-print('might need to run: chmod 400 cpug.pem')
-print('type: ssh -i cpug.pem ec2-user@' + ip_address)
+print('running (and installing web server)')
+print('type: open http://' + ip_address)
